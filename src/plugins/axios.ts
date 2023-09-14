7205767e72b767390aa768bcf8535dc0ae5a2634
Mahmoud <mahmoud.ziad.alsaeid@gmail.com>
2023-09-14 04:30:57 +0400

    initialize axios

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -1,11 +1,45 @@
-import axios from 'axios'
+import axios from "axios";
 
 const axiosIns = axios.create({
+  baseURL: "https://dash.sadeemlight.com/dash/api/v1",
   // You can add your headers here
   // ================================
   // baseURL: 'https://some-domain.com/api/',
   // timeout: 1000,
   // headers: {'X-Custom-Header': 'foobar'}
-})
+});
 
-export default axiosIns
+function convertKeysToCamelCase(obj: any): any {
+  if (typeof obj !== "object" || obj === null) {
+    return obj;
+  }
+  if (Array.isArray(obj)) {
+    return obj.map((item) => convertKeysToCamelCase(item));
+  }
+  const camelCaseObj: any = {};
+  for (const key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      const camelCaseKey =
+        key === "_id" ? "id" : key.replace(/_./g, (x) => x[1].toUpperCase());
+      camelCaseObj[camelCaseKey] = convertKeysToCamelCase(obj[key]);
+    }
+  }
+  return camelCaseObj;
+}
+
+axiosIns.defaults.transformResponse = function (data: any, headers: any) {
+  if (typeof data !== "string") return data;
+  try {
+    const parsedData = JSON.parse(data);
+    const response = convertKeysToCamelCase(parsedData);
+    return response;
+  } catch (error) {
+    return data;
+  }
+};
+
+export const addTokenToAxios = (token: string) => {
+  axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+};
+
+export default axiosIns;
